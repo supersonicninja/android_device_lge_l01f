5aeb1e6375209c141810ab4e26957c62953b8950
supersonicninjaxda <xdaninja@yahoo.co.jp>
2015-10-08 20:58:30 +0900

    import cm-13.0 files from d802, add vendorsetup and gitignore

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,12 +21,3 @@ TARGET_KERNEL_CONFIG := cyanogenmod_l01f_defconfig
 TARGET_OTA_ASSERT_DEVICE := l01f,l-01f,L01F,L-01F,g2,galbi
 
 G2_DTS_TARGET := msm8974-g2-dcm
-
-#Following options are for the TWRP recovery. Delete # if you build TWRP recovery.
-#PRODUCT_COPY_FILES += device/lge/l01f/twrp.fstab:recovery/root/etc/twrp.fstab
-#RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#TW_INTERNAL_STORAGE_PATH := "/data/media"
-#TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-#RECOVERY_SDCARD_ON_DATA := true
-#TW_NO_BATT_PERCENT := true
-#DEVICE_RESOLUTION := 1080x1920
--- l01f.mk
+++ l01f.mk
@@ -17,17 +17,14 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
-
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 PRODUCT_DEVICE := l01f
 PRODUCT_NAME := full_l01f
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := L-01F
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-L01F
+PRODUCT_MANUFACTURER := lge
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from hardware-specific part of the product configuration
